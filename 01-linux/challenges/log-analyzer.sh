861b622467bbb0b6f72fa69e069db50086498779
avila <ajayjaggaiahgari@gmail.com>
2026-01-02 12:54:20 +0530

    adding modified file

--- 01-linux/challenges/log-analyzer.sh
+++ 01-linux/challenges/log-analyzer.sh
@@ -1,25 +1,67 @@
 #!/bin/bash
 
-# Analyze log file for errors
-LOG_FILE=${1:-"C:\Users\Ajay\Desktop\fullstack-workshop\01-linux\sample.txt"}
+set -e
 
+# Analyze log file for errors
+LOG_FILE=${1:-"C:\Users\DELL\Desktop\Sample-log\sample.txt"}
+
+#validate file existence
 if [ ! -f "$LOG_FILE" ]; then
     echo "Error: Log file not found: $LOG_FILE"
     exit 1
 fi
 
-echo "=== Log Analysis Report ==="
+echo "====== Log Analysis Report ======="
 echo "File: $LOG_FILE"
 echo ""
 
-echo "total no.of lines: $(wc -l < "$LOG_FILE")"
-echo "Error count: $(grep -c -i 'error' "$LOG_FILE")"
-echo "Warning count: $(grep -c -i 'warning' "$LOG_FILE")"
-echo "Info count: $(grep -c -i 'info' "$LOG_FILE")"
+#total lines
+total_lines=$(awk 'END {print NR}' "$LOG_FILE")
+echo "Total Lines: $total_lines"
+echo "-------------------------------"
+
+#log levels
+echo "INFO: $(grep -c 'INFO' "$LOG_FILE")"
+echo "ERROR: $(grep -c  'ERROR' "$LOG_FILE")"
+echo "WARNING: $(grep -c  'WARNING' "$LOG_FILE")"
+
 echo ""
+echo "--------------------------------"
 
-echo "=== Recent Errors ==="
-grep -i 'error' "$LOG_FILE" | tail -3
+#extract unique IPs.
+IP_LIST=$(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "$LOG_FILE" | sort | uniq)
 
-echo "=== Unique IP Addresses ==="
-grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' "$LOG_FILE" | sort -u
+echo "Unique IP Addresses Found:"
+for ip in $IP_LIST;
+do 
+	echo " -$ip"
+done
+
+echo "==================================="
+cd
+
+# #!/bin/bash
+
+# # Analyze log file for errors
+# LOG_FILE=${1:-"C:\Users\Ajay\Desktop\fullstack-workshop\01-linux\sample.txt"}
+
+# if [ ! -f "$LOG_FILE" ]; then
+#     echo "Error: Log file not found: $LOG_FILE"
+#     exit 1
+# fi
+
+# echo "=== Log Analysis Report ==="
+# echo "File: $LOG_FILE"
+# echo ""
+
+# echo "total no.of lines: $(wc -l < "$LOG_FILE")"
+# echo "Error count: $(grep -c -i 'error' "$LOG_FILE")"
+# echo "Warning count: $(grep -c -i 'warning' "$LOG_FILE")"
+# echo "Info count: $(grep -c -i 'info' "$LOG_FILE")"
+# echo ""
+
+# echo "=== Recent Errors ==="
+# grep -i 'error' "$LOG_FILE" | tail -3
+
+# echo "=== Unique IP Addresses ==="
+# grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b' "$LOG_FILE" | sort -u
